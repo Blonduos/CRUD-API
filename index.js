e3573657c6840bdcb8f7590bd7f37478f07fadc0
Blonduos <52170374+Blonduos@users.noreply.github.com>
2019-06-30 20:49:11 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const products = require('./routes/products');
+const app = express();
+
+mongoose.connect('mongodb://localhost/proxima')
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDb', err));
+
+app.use(express.json());
+app.use('/api/products', products);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
